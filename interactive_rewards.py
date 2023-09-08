32968c9ea7193c410db8295accc480d069cf916d
Josh Clsn <59794857+javages@users.noreply.github.com>
2023-09-08 15:25:48 -0400

    Add files via upload

---
+++ interactive_rewards.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import plotly.express as px
+
+def enhanced_extract_data(filename):
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    data = []
+    idx = 0
+    while idx < len(lines):
+        if "Global (UTC) Timestamp:" in lines[idx]:
+            timestamp = lines[idx].split(": ", 1)[1].strip()
+            entry_data = {"Global (UTC) Timestamp": timestamp}
+            
+            idx += 1
+            while idx < len(lines) and "------------------------------------------" not in lines[idx]:
+                if ": " in lines[idx]:
+                    key, value = lines[idx].split(": ", 1)
+                    entry_data[key] = value.strip()
+                idx += 1
+            
+            required_keys = ["Global (UTC) Timestamp", "Node", "PID", "Memory used", "Records", 
+                             "Disk usage", "CPU usage", "File descriptors", "Rewards balance"]
+            if all(k in entry_data for k in required_keys):
+                node = entry_data["Node"]
+                pid = entry_data["PID"]
+                try:
+                    memory = float(entry_data["Memory used"].replace("MB", "").strip())
+                except ValueError:
+                    memory = 0.0
+                
+                records = int(entry_data["Records"])
+                disk_usage = float(entry_data["Disk usage"].replace("MB", "").strip() or 0)
+                cpu_usage = float(entry_data["CPU usage"].replace("%", "").strip() or 0)
+                file_descriptors = int(entry_data["File descriptors"] or 0)
+                rewards_balance = float(entry_data["Rewards balance"] or 0)
+                
+                data.append([timestamp, node, pid, memory, records, disk_usage, 
+                             cpu_usage, file_descriptors, rewards_balance])
+        else:
+            idx += 1
+
+    df = pd.DataFrame(data, columns=["Timestamp", "Node", "PID", "Memory", "Records", 
+                                    "Disk usage", "CPU usage", "File descriptors", "Rewards balance"])
+    df["Timestamp"] = pd.to_datetime(df["Timestamp"], format='%a %b %d %H:%M:%S %Z %Y', errors='coerce')
+    if df["Timestamp"].dt.tz is None:
+        df["Timestamp"] = df["Timestamp"].dt.tz_localize('UTC')
+    return df
+
+# Visualization
+custom_hovertemplate = "<br>" + \
+                       "Records: %{customdata[0]}<br>" + \
+                       "Disk usage: %{customdata[1]}MB<br>" + \
+                       "Memory used: %{customdata[2]:.2f}MB<br>" + \
+                       "CPU usage: %{customdata[3]:.2f}%<br>" + \
+                       "File descriptors: %{customdata[4]}<br>" + \
+                       "Rewards balance: %{customdata[5]:.9f}<br>"
+
+def visualize(df):
+    fig = px.line(df, x="Timestamp", y="Rewards balance", color="PID", line_group="PID",
+                  custom_data=["Records", "Disk usage", "Memory", "CPU usage", 
+                               "File descriptors", "Rewards balance"],
+                  title="Rewards Balance Over Time for Each Node", 
+                  labels={"Rewards balance": "Rewards Balance"})
+
+    for trace in fig.data:
+        trace.hovertemplate = custom_hovertemplate
+
+    fig.update_layout(
+        hovermode='y unified',
+        paper_bgcolor='darkgrey',
+        plot_bgcolor='#dee0df',
+        margin=dict(t=42, b=42, l=42, r=42, pad=2),
+        xaxis=dict(showgrid=True, gridcolor='lightgrey', gridwidth=0.1),
+        yaxis=dict(showgrid=True, gridcolor='lightgrey', gridwidth=0.1)
+    )
+    # Modify as needed.
+    output_html_path = "/home/josh/rewards_balance_plot.html"
+    fig.write_html(output_html_path)
+
+# Modify as needed.
+df = enhanced_extract_data("/home/josh/resources.log")
+visualize(df)
+
+
---
+++ resources.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+base_dir="${HOME}/.local/share/safe/node"
+declare -A dir_pid
+node_number=0
+
+for dir in "$base_dir"/*; do
+  if [[ -f "$dir/safenode.pid" ]]; then
+    dir_name=$(basename "$dir")
+    dir_pid["$dir_name"]=$(cat "$dir/safenode.pid")
+  fi
+done
+
+for dir in "${!dir_pid[@]}"; do
+  pid=${dir_pid[$dir]}
+
+  echo "------------------------------------------"
+  echo "Local Timestamp: $(date +'%a %b %d %H:%M:%S %Z %Y')"
+  echo "Global (UTC) Timestamp: $(TZ='UTC' date +'%a %b %d %H:%M:%S %Z %Y')"
+  echo "Number: $node_number"
+  echo "Node: $dir"
+  echo "PID: $pid"
+
+  process_info=$(ps -o rss,%cpu -p $pid | awk 'NR>1')
+  if [[ -n "$process_info" ]]; then
+      status="running"
+      mem_used=$(echo "$process_info" | awk '{print $1/1024 "MB"}')
+      cpu_usage=$(echo "$process_info" | awk '{print $2"%"}')
+  else
+      status="killed"
+  fi
+
+  echo "Status: $status"
+  echo "Memory used: $mem_used"
+  echo "CPU usage: $cpu_usage"
+
+  file_descriptors=$(ls /proc/$pid/fd/ 2>/dev/null | wc -l)
+  echo "File descriptors: $file_descriptors"
+
+  record_store_dir="$base_dir/$dir/record_store"
+  if [ -d "$record_store_dir" ]; then
+    records=$(ls -1 $record_store_dir | wc -l)
+    echo "Records: $records"
+
+    disk_usage=$(du -sh "$record_store_dir" | awk '{print $1}' | sed 's/M/MB/')
+    echo "Disk usage: $disk_usage"
+  else
+    echo "$dir does not contain record_store"
+  fi
+
+  rewards_balance=$(${HOME}/.local/bin/safe wallet balance --peer-id="$dir" | grep -oP '(?<=: )\d+\.\d+')
+  echo "Rewards balance: $rewards_balance"
+
+  echo
+  node_number=$((node_number + 1))
+done
+
+echo "------------------------------------------"
+
+
+
+
+
+
